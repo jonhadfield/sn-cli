7e4e8542beca23ae7bbc2d37a0530cabac29ca41
Jon Hadfield <jon@lessknown.co.uk>
2020-06-15 14:23:48 +0100

    improve syncing status output.

--- sync.go
+++ sync.go
@@ -9,12 +9,12 @@ import (
 
 func Sync(si cache.SyncInput, showProgress bool) (so cache.SyncOutput, err error) {
 	if showProgress && !si.Debug {
-		prefix := "syncing "
+		prefix := HiWhite("syncing ")
 		if _, err := os.Stat(si.Session.CacheDBPath); os.IsNotExist(err) {
-			prefix = "initialising "
+			prefix = HiWhite("initialising ")
 		}
 
-		s := spinner.New(spinner.CharSets[9], 100*time.Millisecond) // Build our new spinner
+		s := spinner.New(spinner.CharSets[14], 100*time.Millisecond) // Build our new spinner
 		s.Prefix = prefix
 		s.Start()
 		so, err = sync(si)
