3acad3b709342a15e83718e64b799447c4662aff
Jon Hadfield <jon@lessknown.co.uk>
2020-06-21 13:08:13 +0100

    externalise delete tags code.

--- cmd/sncli/main.go
+++ cmd/sncli/main.go
@@ -368,47 +368,7 @@ func startCLI(args []string) (msg string, useStdOut bool, err error) {
 							return err
 						}
 						useStdOut = opts.useStdOut
-
-						titleIn := strings.TrimSpace(c.String("title"))
-						uuidIn := strings.Replace(c.String("uuid"), " ", "", -1)
-						if titleIn == "" && uuidIn == "" {
-							if cErr := cli.ShowSubcommandHelp(c); cErr != nil {
-								panic(cErr)
-							}
-							return errors.New("title or uuid required")
-						}
-						session, _, err := cache.GetSession(opts.useSession,
-							opts.sessKey, opts.server)
-						if err != nil {
-							return err
-						}
-						tags := sncli.CommaSplit(titleIn)
-						uuids := sncli.CommaSplit(uuidIn)
-
-						var cacheDBPath string
-						cacheDBPath, err = cache.GenCacheDBPath(session, opts.cacheDBDir, snAppName)
-						if err != nil {
-							return err
-						}
-						session.CacheDBPath = cacheDBPath
-
-						DeleteTagConfig := sncli.DeleteTagConfig{
-							Session:   session,
-							TagTitles: tags,
-							TagUUIDs:  uuids,
-							Debug:     opts.debug,
-						}
-						var noDeleted int
-						noDeleted, err = DeleteTagConfig.Run()
-						if err != nil {
-							return fmt.Errorf("failed to delete tag. %+v", err)
-						}
-
-						if noDeleted > 0 {
-							msg = sncli.Green(fmt.Sprintf("%s tag", msgDeleted))
-						} else {
-							msg = sncli.Yellow("Tag not found")
-						}
+						msg, err = processDeleteTags(c, opts)
 
 						return nil
 					},
---
+++ cmd/sncli/tag.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jonhadfield/gosn-v2/cache"
+	sncli "github.com/jonhadfield/sn-cli"
+	"github.com/urfave/cli"
+	"strings"
+)
+
+func processDeleteTags(c *cli.Context, opts configOptsOutput) (msg string, err error) {
+	titleIn := strings.TrimSpace(c.String("title"))
+	uuidIn := strings.Replace(c.String("uuid"), " ", "", -1)
+	if titleIn == "" && uuidIn == "" {
+		if cErr := cli.ShowSubcommandHelp(c); cErr != nil {
+			panic(cErr)
+		}
+		return "", errors.New("title or uuid required")
+	}
+	session, _, err := cache.GetSession(opts.useSession,
+		opts.sessKey, opts.server)
+	if err != nil {
+		return "", err
+	}
+	tags := sncli.CommaSplit(titleIn)
+	uuids := sncli.CommaSplit(uuidIn)
+
+	var cacheDBPath string
+	cacheDBPath, err = cache.GenCacheDBPath(session, opts.cacheDBDir, snAppName)
+	if err != nil {
+		return "", err
+	}
+	session.CacheDBPath = cacheDBPath
+
+	DeleteTagConfig := sncli.DeleteTagConfig{
+		Session:   session,
+		TagTitles: tags,
+		TagUUIDs:  uuids,
+		Debug:     opts.debug,
+	}
+	var noDeleted int
+	noDeleted, err = DeleteTagConfig.Run()
+	if err != nil {
+		return "", fmt.Errorf("failed to delete tag. %+v", err)
+	}
+
+	if noDeleted > 0 {
+		msg = sncli.Green(fmt.Sprintf("%s tag", msgDeleted))
+	} else {
+		msg = sncli.Yellow("Tag not found")
+	}
+
+	return "", err
+}
