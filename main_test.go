6c14f8408680c304b14fcbcfc96f93f4aca765e5
Jon Hadfield <jon@lessknown.co.uk>
2020-06-24 22:47:57 +0100

    remove unused code.

--- main_test.go
+++ main_test.go
@@ -3,16 +3,6 @@ package sncli
 import (
 	"os"
 	"strings"
-
-	"github.com/jonhadfield/gosn-v2"
-)
-
-var (
-	sInput = gosn.SignInInput{
-		Email:     os.Getenv("SN_EMAIL"),
-		Password:  os.Getenv("SN_PASSWORD"),
-		APIServer: os.Getenv("SN_SERVER"),
-	}
 )
 
 func removeDB(dbPath string) {
