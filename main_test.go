a894d3f0ce522c44990cacadc10a682201d4c8b9
Jon Hadfield <jon@lessknown.co.uk>
2021-11-26 12:41:22 +0000

    allow for different missing file error for windows environments.

--- main_test.go
+++ main_test.go
@@ -2,13 +2,19 @@ package sncli
 
 import (
 	"os"
+	"runtime"
 	"strings"
 	"time"
 )
 
 func removeDB(dbPath string) {
 	if err := os.Remove(dbPath); err != nil {
-		if !strings.Contains(err.Error(), "no such file or directory") {
+		if StringInSlice(runtime.GOOS, []string{"linux", "darwin"}, false) {
+			if !strings.Contains(err.Error(), "no such file or directory") {
+				panic(err)
+			}
+		}
+		if runtime.GOOS == "windows" && !strings.Contains(err.Error(), "cannot find the file specified") {
 			panic(err)
 		}
 	}
