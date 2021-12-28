25224f54ed6d3fa53362bd1e4754acc0f7b8a434
Jon Hadfield <jon@lessknown.co.uk>
2021-12-28 17:29:30 +0000

    handle json export and import.

--- helpers.go
+++ helpers.go
@@ -49,8 +49,44 @@ func writeGob(filePath string, object interface{}) error {
 	return err
 }
 
-func writeJSON(filePath string, object interface{}) error {
-	file, err := os.Create(filePath)
+type EncryptedItemExport struct {
+	UUID               string  `json:"uuid"`
+	ItemsKeyID         *string `json:"items_key_id,omitempty"`
+	Content            string  `json:"content"`
+	ContentType        string  `json:"content_type"`
+	Deleted            bool    `json:"deleted"`
+	EncItemKey         string  `json:"enc_item_key"`
+	CreatedAt          string  `json:"created_at"`
+	UpdatedAt          string  `json:"updated_at"`
+	CreatedAtTimestamp int64   `json:"created_at_timestamp"`
+	UpdatedAtTimestamp int64   `json:"updated_at_timestamp"`
+	DuplicateOf        *string `json:"duplicate_of"`
+}
+
+func writeJSON(i ExportConfig, items gosn.EncryptedItems) error {
+	// prepare for export
+	var itemsExport []EncryptedItemExport
+	for x := range items {
+		if items[x].DuplicateOf != nil {
+			return fmt.Errorf("unexpected duplicate: %s", *items[x].DuplicateOf)
+		}
+
+		itemsExport = append(itemsExport, EncryptedItemExport{
+			UUID:               items[x].UUID,
+			ItemsKeyID:         items[x].ItemsKeyID,
+			Content:            items[x].Content,
+			Deleted:            items[x].Deleted,
+			ContentType:        items[x].ContentType,
+			EncItemKey:         items[x].EncItemKey,
+			CreatedAt:          items[x].CreatedAt,
+			UpdatedAt:          items[x].UpdatedAt,
+			CreatedAtTimestamp: items[x].CreatedAtTimestamp,
+			UpdatedAtTimestamp: items[x].UpdatedAtTimestamp,
+			DuplicateOf:        items[x].DuplicateOf,
+		})
+	}
+
+	file, err := os.Create(i.File)
 	if err != nil {
 		return err
 	}
@@ -59,12 +95,24 @@ func writeJSON(filePath string, object interface{}) error {
 
 	var jsonExport []byte
 	if err == nil {
-		jsonExport, err = json.MarshalIndent(object,"","  ")
+		jsonExport, err = json.MarshalIndent(itemsExport, "", "  ")
 	}
 
 	content := strings.Builder{}
-	content.WriteString("{\n  \"items\": ")
+	content.WriteString("{\n  \"version\": \"004\",")
+	content.WriteString("\n  \"items\": ")
 	content.WriteString(string(jsonExport))
+	content.WriteString(",")
+
+	// add keyParams
+	content.WriteString("\n  \"keyParams\": {")
+	content.WriteString(fmt.Sprintf("\n    \"identifier\": \"%s\",", i.Session.KeyParams.Identifier))
+	content.WriteString(fmt.Sprintf("\n    \"version\": \"%s\",", i.Session.KeyParams.Version))
+	content.WriteString(fmt.Sprintf("\n    \"origination\": \"%s\",", i.Session.KeyParams.Origination))
+	content.WriteString(fmt.Sprintf("\n    \"created\": \"%s\",", i.Session.KeyParams.Created))
+	content.WriteString(fmt.Sprintf("\n    \"pw_nonce\": \"%s\"", i.Session.KeyParams.PwNonce))
+	content.WriteString("\n  }")
+
 	content.WriteString("\n}")
 
 	_, err = file.WriteString(content.String())
@@ -84,17 +132,26 @@ func readGob(filePath string, object interface{}) error {
 	return err
 }
 
-func readJSON(filePath string, items *gosn.EncryptedItems) error {
+type EncryptedItemsFile struct {
+	Items gosn.EncryptedItems `json:"items"`
+}
+
+func readJSON(filePath string) (items gosn.EncryptedItems, err error) {
 	file, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return fmt.Errorf("failed to open: %s", filePath)
-	}
-	err = json.Unmarshal(file, &items)
-	if err != nil {
-		return fmt.Errorf("failed to unmarshall json: %w", err)
+		err = fmt.Errorf("%w failed to open: %s", err, filePath)
+		return
 	}
 
-	return err
+	var eif EncryptedItemsFile
+
+	err = json.Unmarshal(file, &eif)
+	if err != nil {
+		err = fmt.Errorf("failed to unmarshall json: %w", err)
+		return
+	}
+
+	return eif.Items, err
 }
 
 func ItemRefsToYaml(irs []gosn.ItemReference) []ItemReferenceYAML {
