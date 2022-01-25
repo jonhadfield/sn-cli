5e0341db2a5e286357b1e91eeaa1039a1033819a
Jon Hadfield <jon@lessknown.co.uk>
2022-01-25 21:19:16 +0000

    fix sign in call for tests.

--- note_test.go
+++ note_test.go
@@ -56,7 +56,7 @@ func TestMain(m *testing.M) {
 	if os.Getenv("SN_SERVER") == "" || strings.Contains(os.Getenv("SN_SERVER"), "ramea") {
 		localTestMain()
 	} else {
-		signIn(os.Getenv("SN_EMAIL"), os.Getenv("SN_PASSWORD"), os.Getenv("SN_SERVER"))
+		signIn(os.Getenv("SN_SERVER"), os.Getenv("SN_EMAIL"), os.Getenv("SN_PASSWORD"))
 	}
 
 	if _, err := gosn.Sync(gosn.SyncInput{Session: gTtestSession}); err != nil {
