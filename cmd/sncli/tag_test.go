85dd6f260fc8a457cd6c76f9b41e6ccd729c8f08
Jon Hadfield <jon@lessknown.co.uk>
2023-08-27 11:31:40 +0100

    use ramea if local only.

--- cmd/sncli/tag_test.go
+++ cmd/sncli/tag_test.go
@@ -53,7 +53,7 @@ func signIn(server, email, password string) {
 }
 
 func TestMain(m *testing.M) {
-	if os.Getenv("SN_SERVER") == "" || strings.Contains(os.Getenv("SN_SERVER"), "ramea") {
+	if strings.Contains(os.Getenv("SN_SERVER"), "ramea") {
 		localTestMain()
 	} else {
 		signIn(os.Getenv("SN_SERVER"), os.Getenv("SN_EMAIL"), os.Getenv("SN_PASSWORD"))
